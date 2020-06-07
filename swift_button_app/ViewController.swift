8cd43b7018e59ba9384d3d6b9e4719e8b7b81637
Haya <max9173627@gmail.com>
2020-06-07 12:18:50 +0900

    first commit

---
+++ swift_button_app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swift_button_app
+//
+//  Created by haya on 2020/06/07.
+//  Copyright © 2020 haya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    var count = 10
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        count = 10
+        print(count)
+    }
+
+
+}
+
